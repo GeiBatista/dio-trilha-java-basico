30914b78d795ef3a776dfd2dc218cb4f0987c82b
geibatista <geibatista442@gmail.com>
2024-07-29 22:30:22 -0300

    Entrega de desafio conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner entrada = new Scanner(System.in); 
+
+        System.out.println("Por favor, digite o número da Agência !");
+        int numero = entrada.nextInt();
+        
+        System.out.println("Por favor, digite o código da Agência !");
+        String codigo = entrada.next();
+
+        System.out.println("Por favor, informe o nome do cliente !");
+        String nome = entrada.next();
+
+        System.out.println("Por favor, informe o sobrenome do cliente !");
+        String sobrenome = entrada.next();
+
+        System.out.println("Por favor, digite o saldo da conta !");
+        double saldo = entrada.nextDouble();
+
+        System.out.println("Olá " + nome + " " + sobrenome + ", obrigado por criar uma conta em nosso banco, sua agência é " + codigo +", conta " + numero +  " e seu saldo " + saldo + " já está disponível para saque.");
+        
     }
 }
